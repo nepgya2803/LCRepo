4462e89fa3c837e549b32bc1cdcc756f1e506741
supnep <44500850+nepgya2803@users.noreply.github.com>
2024-06-07 08:34:54 +0000

    Sync LeetCode submission - Two Sum (csharp)

--- none/problems/two_sum/solution.cs
+++ none/problems/two_sum/solution.cs
@@ -1,18 +1,15 @@
 public class Solution {
-   public int[] TwoSum(int[] nums, int target)
-    {
-        var maps = new Dictionary<int, int>();
-        for (var i = 0; i < nums.Count(); i++)
-        {
-            var diff = target - nums[i];
-            if (maps.ContainsKey(diff))
-            {
-                return [i, maps[diff]];
-            }
-
-            if (!maps.ContainsKey(nums[i])) maps.Add(nums[i], i);
-        }
-
-        return [0, 0];
+    public int[] TwoSum(int[] nums, int target) {
+         var dictionary = new Dictionary<int, int>();
+  for (int i = 0; i < nums.Length; i++)
+  {
+      int result = target - nums[i];
+      if (dictionary.ContainsKey(result))
+      {
+          return new int[] { dictionary[result],i };
+      }
+       dictionary[nums[i]] = i;
+  }
+        throw new ArgumentException("not find target");
     }
 }
