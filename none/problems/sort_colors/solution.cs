89871efc507da293ff9c696d29deed02da1f41a4
supnep <44500850+nepgya2803@users.noreply.github.com>
2024-06-14 10:16:01 +0000

    Sync LeetCode submission - Sort Colors (csharp)

--- none/problems/sort_colors/solution.cs
+++ none/problems/sort_colors/solution.cs
@@ -1,5 +1,20 @@
 public class Solution {
     public void SortColors(int[] nums) {
-        Array.Sort(nums);
+        int j = 0, k = nums.Length - 1;
+        for(int i = 0; i <= k ; i++)
+        {
+            if(nums[i] == 0)
+            {
+                (nums[i], nums[j]) = (nums[j], nums[i]);
+                j++;
+            }
+
+            if(nums[i] == 2)
+            {
+                (nums[i], nums[k]) = (nums[k], nums[i]);
+                i--;
+                k--;
+            }
+        }
     }
 }
