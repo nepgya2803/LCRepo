0b42febbd8699f210f5e628768bc89b7e7931704
supnep <44500850+nepgya2803@users.noreply.github.com>
2024-06-17 04:44:05 +0000

    Sync LeetCode submission - Sum of Square Numbers (csharp)

---
+++ none/problems/sum_of_square_numbers/solution.cs
@@ -0,0 +1,25 @@
+public class Solution {
+    public bool JudgeSquareSum(int c) {
+        long left = 0;
+        long right = (long)Math.Sqrt(c);
+
+        while(left <= right)
+        {
+            long sum = left*left + right*right;
+            if(sum == c)
+            {
+                return true;
+            }
+            else if(sum < c)
+            {
+                left++;
+            }
+            else
+            {
+                right--;
+            }
+        }
+
+        return false;
+    }
+}
